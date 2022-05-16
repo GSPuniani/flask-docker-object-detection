9f3b9e9a47b81f482d3926f97114c068e4180831
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-16 00:24:47 -0700

    Added object detection model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # import required modules
 from flask import Flask, render_template, request
+from src.utils import *
+from src.detect import *
 
 # create flask app
 app = Flask(__name__)
@@ -16,7 +18,10 @@ def upload():
     file = request.files['file']
     filepath = f"static/temp/{file.filename}"
     file.save(filepath) # save to directory
-    
+
+    # process image after upload
+    process_image(filepath)
+
     # return server url to client
     return f"{request.url_root}{filepath}"
 
